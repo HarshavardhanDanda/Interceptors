d9942331fd953009de67a66dffba57b592a328d6
Harshazemoso <harshavardhan.danda@zemosolabs.com>
2022-06-06 15:25:33 +0530

    interceptors

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// For GET requests
+axios.interceptors.request.use(
+   (req) => {
+console.log(req);
+      return req;
+   },
+   (err) => {
+      return Promise.reject(err);
+   }
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// For POST requests
+axios.interceptors.response.use(
+   (res) => {
+      // Add configurations here
+      if (res.status === 201) {
+         console.log('Posted Successfully');
+      }
+      console.log(res);
+      return res;
+   },
+   (err) => {
+      return Promise.reject(err);
+   }
+);
+
+ReactDOM.render(
+   <React.StrictMode>
+      <App />
+   </React.StrictMode>,
+   document.getElementById('root')
+);
